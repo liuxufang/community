35e72b9af207dfb14b103b1aa35e4e68223d6af8
unknown <liuxufang521@163.com>
2019-11-27 21:19:59 +0800

    init repo

---
+++ src/test/java/com/liu/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.community;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommunityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
